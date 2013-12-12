ff63557d145e1b7265bc46d4d321a60828594d46
Jacek Wielemborek <d33tah@gmail.com>
2013-12-13 00:10:06 +0100

    Keep track of the visit times.

--- macd.py
+++ macd.py
@@ -9,6 +9,7 @@ import collections
 NETWORK = "172.16.1.1/24"
 OUTFILE = "index.html"
 INTERVAL = 60
+TIMEOUT = 60 * 30
 
 def get_macs(network, do_sudo=True):
     ret = []
@@ -31,24 +32,39 @@ def load_known():
             ret[mac] = name
     return ret
 
-def write_macs(macs, known, filename=OUTFILE):
+def get_since_time(since, mac):
+    since_time = since[mac]
+    if time.strftime("%x") == time.strftime("%x", since_time):
+        return "(od %s)" % time.strftime("%X", since_time)
+    else:
+        return "(od %s)" % time.strftime("%x %X", since_time)
+
+def write_macs(macs, known, since, filename=OUTFILE):
     with open(filename, "w") as f:
         f.write(time.strftime("%x %X<br/>\n<br/>\n"))
         for mac in macs:
+            since_msg = get_since_time(since, mac)
             if mac in known:
-                f.write("%s<br/>\n" % known[mac])
+                f.write("%s %s<br/>\n" % (known[mac], since_msg))
             else:
-                f.write("%s (?)<br/>\n" % mac)
+                f.write("%s %s(?)<br/>\n" % (mac, since_msg))
 
+def cleanup_last_seen(macs, last_seen, since):
+    for mac in macs:
+        if mac in last_seen and time.time() - last_seen[mac] > TIMEOUT:
+            since[mac] = time.gmtime()
+        else:
+            last_seen[mac] = time.time()
 
 def main():
     FORMAT = "%(asctime)-15s %(message)s"
     logging.basicConfig(format=FORMAT, level=logging.INFO)
-    since = collections.defaultdict(time.time)
-    last_visible = {}
+    since = collections.defaultdict(time.gmtime)
+    last_seen = {}
     while True:
         known = load_known()
         macs = get_macs(NETWORK)
+        cleanup_last_seen(macs, last_seen, since)
         logging.info("%s" % macs)
         write_macs(macs, known, since)
         time.sleep(INTERVAL)
