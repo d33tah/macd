3f86178fbc8df66e47ab5de46b484f691665cfd6
Jacek Wielemborek <d33tah@gmail.com>
2013-12-21 02:03:20 +0100

    Actually use the Device.__str__ when displaying the device list.

--- macd/views.py
+++ macd/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 def index(request):
     time_threshold = datetime.date.today() - datetime.timedelta(hours=3)
     items = SeenEvent.objects.filter(date__gte=time_threshold)
-    devices = set(item.mac.device.description for item in items
+    devices = set(str(item.mac.device) for item in items
                   if not item.mac.device.ignored)
     return render(request, 'macd/index.html', {
         'devices': devices,
